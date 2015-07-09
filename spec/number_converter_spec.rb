22d22f8f19ccc87dbfc9e81139c478a8ff162712
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-07-09 23:07:42 +0530

    Add test to instantiate new class object

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'number_converter'
 
 describe NumberConverter do
-  it "has some tests"
+  it "can instantiate new objects" do
+    expect(NumberConverter.new).to_not be(nil)
+  end
 end
-
