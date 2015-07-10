ed9ded6cd52318455e18e94dd89460cd68360b74
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-07-10 19:49:51 +0530

    Refactor tests to remove redundancy
    This style of testing seems to be appropriate for now.

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -4,9 +4,23 @@ require 'number_converter'
 describe NumberConverter do
   let(:converter) { NumberConverter.new }
 
-  context "when the input is 0" do
-    it "returns an empty string" do
-      expect(converter.convert(0)).to eq("")
+  describe '#convert' do
+    context "when the input is 0" do
+      it "returns an empty string" do
+        expect(converter.convert(0)).to eq("")
+      end
+    end
+
+    {
+        1 => 'I',
+        2 => 'II',
+    }.each do |input, expected|
+       context "For the input #{input}" do
+        it "returns #{expected}" do
+          expect(converter.convert(input)).to eq expected
+        end
+      end
+
     end
   end
 end
