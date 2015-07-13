4e54f889a014c1136d495af9357f126cbd10c8d0
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-07-14 00:35:43 +0530

    Add tests for conversion upto 100
    The code involves recursion and I am not sure about it being the best technique. Although, as of now, I am not able to think of any better way to avoid multiple if-else statements. Moreover recursion very well describes the behavior of roman numerals.

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -3,21 +3,31 @@ class NumberConverter
   def convert(arabic_number)
 
     if arabic_number == 0
-      ""
-    elsif arabic_number <= 3
-      "I" + convert(arabic_number-1)
+       ""
     elsif arabic_number == 4
-      "I" + convert(5)
-    elsif arabic_number <= 8
-      "V" + convert(arabic_number - 5)
-    elsif arabic_number == 10
-      "X"
+      "IV"
     elsif arabic_number == 9
-      "I" + convert(10)
+      "IX"
+    elsif arabic_number == 100
+      "C"
+    elsif arabic_number >= 1 && arabic_number  <=  3
+      "I"  + convert(arabic_number - 1)
+    elsif arabic_number >= 5 && arabic_number  <=  8
+      "V"  + convert(arabic_number - 5)
+    elsif arabic_number >= 10 && arabic_number <= 39
+      "X"  + convert(arabic_number - 10)
+    elsif arabic_number >= 40 && arabic_number <= 49
+      "XL" + convert(arabic_number - 40)
+    elsif arabic_number >= 50 && arabic_number <= 89
+      "L"  + convert(arabic_number - 50)
+    elsif arabic_number >= 90 && arabic_number <= 99
+      "XC" + convert(arabic_number - 90)
 
     end
+
   end
 
+  
 end
 
 
