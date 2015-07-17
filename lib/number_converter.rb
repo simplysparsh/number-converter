0490c4df3678a9a0ebb8a427e02944cf299c8f17
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-07-17 20:02:43 +0530

    Use case statement instead of if-else

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -2,32 +2,20 @@ class NumberConverter
 
   def convert(arabic_number)
 
-    if arabic_number == 0
-       ""
-    elsif arabic_number == 4
-      "IV"
-    elsif arabic_number == 9
-      "IX"
-    elsif arabic_number == 100
-      "C"
-    elsif arabic_number >= 1 && arabic_number  <=  3
-      "I"  + convert(arabic_number - 1)
-    elsif arabic_number >= 5 && arabic_number  <=  8
-      "V"  + convert(arabic_number - 5)
-    elsif arabic_number >= 10 && arabic_number <= 39
-      "X"  + convert(arabic_number - 10)
-    elsif arabic_number >= 40 && arabic_number <= 49
-      "XL" + convert(arabic_number - 40)
-    elsif arabic_number >= 50 && arabic_number <= 89
-      "L"  + convert(arabic_number - 50)
-    elsif arabic_number >= 90 && arabic_number <= 99
-      "XC" + convert(arabic_number - 90)
-
+    case arabic_number
+      when 0      then ''
+      when 1..3   then 'I'  + convert(arabic_number - 1)
+      when 4      then 'IV'
+      when 5..8   then 'V'  + convert(arabic_number - 5)
+      when 9      then 'IX'
+      when 10..39 then 'X'  + convert(arabic_number - 10)
+      when 40..49 then 'XL' + convert(arabic_number - 40)
+      when 50..89 then 'L'  + convert(arabic_number - 50)
+      when 60..99 then 'XC' + convert(arabic_number - 90)
+      when 100    then 'C'
     end
 
   end
-
-  
 end
 
 
