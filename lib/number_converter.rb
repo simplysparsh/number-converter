9dc42482fa68bb2bae1f199b1997cdb09f8356e2
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-07-20 12:53:49 +0530

    Refactor to have each method perform one task only

--- lib/number_converter.rb
+++ lib/number_converter.rb
@@ -1,28 +1,37 @@
 class NumberConverter
 
-  def convert(arabic_number)
-
-    case arabic_number
-      when 0       then ''
-      else convert_to_roman(arabic_number)
-    end
-
+  def initialize
+  @conversion_factors_in_arabic = [ 1,   4,    5,   9,   10,   40,  50,  90,   100, 400,  500, 900, 1000, @upper_bound]
+  @conversion_factors_in_roman  = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
   end
+
+
+  def convert(arabic)
+    case arabic
+      when 0       then ''
+      else convert_to_roman(arabic)
+    end
+  end
+
+  private
 
   def convert_to_roman(arabic)
-    upper_bound = arabic + 1
-    conversion_factors_in_arabic = [1,   4,    5,   9,   10,   40,  50,  90, 100, 400,  500, 900, 1000, upper_bound]
-    conversion_factors_in_roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+    set_last_conversaion_factor(arabic)
 
-    required_factor = get_conversion_factor(arabic, conversion_factors_in_arabic)
-
-    conversion_factors_in_roman[required_factor] + convert(arabic-conversion_factors_in_arabic[required_factor])
+    required_factor = get_conversion_factor(arabic)
+    @conversion_factors_in_roman[required_factor] + convert(arabic-@conversion_factors_in_arabic[required_factor])
   end
 
-  def get_conversion_factor(arabic, conversion_factors_in_arabic)
+
+  def set_last_conversaion_factor(arabic)
+    @conversion_factors_in_arabic[-1] = arabic + 1
+  end
+
+
+  def get_conversion_factor(arabic)
     array_element = 0
 
-    while arabic >= conversion_factors_in_arabic[array_element +1]
+    while arabic >= @conversion_factors_in_arabic[array_element + 1]
       array_element = array_element + 1
     end
 
